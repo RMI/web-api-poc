6d5ae11b72fae73a50e9a578c659677c70c651d3
Samuel Huestis <samhuestis@gmail.com>
2025-02-24 16:29:02 -0700

    adding integration test for mtcars item

--- tests/test_integration.py
+++ tests/test_integration.py
@@ -1,5 +1,6 @@
 from fastapi.testclient import TestClient
 from main import app
+from models.outputs import mtcar
 
 client = TestClient(app)
 
@@ -8,3 +9,13 @@ def test_health_check():
     response = client.get("/health/")
     assert response.status_code == 200
     assert response.json() == {"status": "OK"}
+
+def test_output_model(): 
+    response = client.get("/api/HondaCivic")
+    assert response.status_code == 200
+    mtcar.model_validate(response.json())
+
+#def test_output_model(): 
+  #  response = client.get("/api/dataset")
+ #   assert response.status_code == 200
+  #  mtcar.model_validate(response.json())
