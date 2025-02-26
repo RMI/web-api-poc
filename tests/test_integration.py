5652bfdd2b766af57bee1071d0ddccf2ef259f49
Samuel Huestis <samhuestis@gmail.com>
2025-02-26 15:48:24 -0700

    new integration test to validate mtcars full dataset, removed json encoding from services as it was not needed with new method, removed mtcarlist data model

--- tests/test_integration.py
+++ tests/test_integration.py
@@ -13,9 +13,14 @@ def test_health_check():
 def test_output_model(): 
     response = client.get("/api/HondaCivic")
     assert response.status_code == 200
+    #Validate single mtcars instance against mtcar model 
     mtcar.model_validate(response.json())
 
-'''def test_output_model(): 
+def test_output_model(): 
     response = client.get("/api/dataset")
     assert response.status_code == 200
-    mtcarlist.model_validate(response.json())'''
+    #Validate that full dataset is a list and each list item is mtcar
+    mtcars = response.json()
+    assert isinstance (mtcars, list)
+    for mtcar_data in mtcars: 
+        mtcar(**mtcar_data)
